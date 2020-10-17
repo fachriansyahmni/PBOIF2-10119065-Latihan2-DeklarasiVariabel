b25fc2e5219dd216b0f769fbb5c859834fa475b8
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-17 12:04:26 +0700

    upload latihan 2

---
+++ src/pboif2_10119065_latihan2_deklarasivariabel/PBOIF2_10119065_Latihan2_DeklarasiVariabel.java
@@ -0,0 +1,34 @@
+package pboif2_10119065_latihan2_deklarasivariabel;
+
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini berisi pendeklarasian dan pengaksesan 
+ *                        variabel
+ *
+ */
+public class PBOIF2_10119065_Latihan2_DeklarasiVariabel {
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+            int nilaiInt;
+            final double PHI = 3.14; //konstanta uppercase
+            boolean nilaiLogika;
+            char nilaiKarakter;
+
+        // Memberi nilai ke variabel
+            nilaiInt = 90;
+            nilaiLogika = false;
+            nilaiKarakter = 'A';
+
+        // Menampilkan hasil
+            System.out.println();
+            System.out.println("Isi variabel nilai = " + nilaiInt);
+            System.out.println("Isi variabel PHI = "+PHI);
+            System.out.println("Isi variabel Logika = "+nilaiLogika);
+            System.out.println("Isi variabel Karakter = "+nilaiKarakter);
+
+    }
+    
+}
